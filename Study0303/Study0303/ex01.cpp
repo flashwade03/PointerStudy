f874f1ff0c381f597689ba346947012926e14718
Jung HeeChang <jgmlckd@gmail.com>
2016-03-08 16:49:52 +0900

    Study Files

---
+++ Study0303/Study0303/ex01.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+class complex{
+	double re, im;
+public:
+	complex(double r, double i){ re = r; im = i; }
+	complex(double r){ re = r; im = 0; }
+	complex(){ re = im = 0; }
+
+	friend complex operator +(complex, complex);
+	friend complex operator -(complex, complex);
+	friend complex operator -(complex);
+	friend complex operator *(complex, complex);
+	friend complex operator /(complex, complex);
+	friend bool operator ==(complex, complex);
+	friend bool operator !=(complex, complex);
+	//...
+};
+
+int main()
+{
+
+}
